9125748d2654d10550a06cbddc7e310010fc11e4
mustafa-bircan <msbrtfcn@gmail.com>
2025-02-06 13:47:03 +0300

    Click Event for React

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,17 +1,33 @@
 function Button() {
 
     const styles = {
-        
-            backgroundColor: "hsl(200, 100%, 50%)",
-            color: "white",
-            padding: "10px 20px",
-            borderRadius: "5px",
-            border: "none",
-            cursor: "pointer",
+
+        backgroundColor: "hsl(200, 100%, 50%)",
+        color: "white",
+        padding: "10px 20px",
+        borderRadius: "5px",
+        border: "none",
+        cursor: "pointer",
     }
 
+    // let count = 0;
+
+    // const handleClick = (name) => {
+    //     if (count < 3) {
+    //         count++;
+    //         console.log(`${name} you clicked me ${count} time/s`)
+    //     }
+    //     else {
+    //         console.log(`${name} stop clicking me!`)
+    //     }
+    // };
+
+    // const handleClick = (name) => console.log(`${name} stop clicking me`)
+
+    const handleClick = (e) => e.target.textContent = 'OOPS!';
+
     return (
-        <button style={styles}>Click me</button>
+        <button style={styles} onDoubleClick={(e) => handleClick(e)}>Click me</button>
     )
 }
 
