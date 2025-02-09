c6acc08be9130320067d8ce45e6dc4136a720456
mustafa-bircan <msbrtfcn@gmail.com>
2025-02-09 15:33:00 +0300

    onChange examples completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,38 @@
 import Button from "./components/Button"
 import Card from "./components/Card"
+import Counter from "./components/Counter"
 
 import Food from "./components/Food"
 import Footer from "./components/Fotoer"
 import Header from "./components/Header"
 import List from "./components/List"
+import MyComponent from "./components/MyComponent"
+import ProfilePicture from "./components/ProfilePicture"
 import Student from "./components/Student"
 import UserGreeting from "./components/UserGreeting"
 
 
 function App() {
 
-  const fruits = [{ id: 1, name: "apple", calories: 95 },
-  { id: 2, name: "orange", calories: 45 },
-  { id: 3, name: "banana", calories: 105 },
-  { id: 4, name: "coconut", calories: 159 },
-  { id: 5, name: "pineapple", calories: 37 }];
+  // const fruits = [{ id: 1, name: "apple", calories: 95 },
+  // { id: 2, name: "orange", calories: 45 },
+  // { id: 3, name: "banana", calories: 105 },
+  // { id: 4, name: "coconut", calories: 159 },
+  // { id: 5, name: "pineapple", calories: 37 }];
 
 
-  const vegetables = [{ id: 6, name: "potatoes", calories: 110 },
-  { id: 7, name: "celery", calories: 15 },
-  { id: 8, name: "carrots", calories: 25 },
-  { id: 9, name: "corn", calories: 63 },
-  { id: 10, name: "broccoli", calories: 50 }];
+  // const vegetables = [{ id: 6, name: "potatoes", calories: 110 },
+  // { id: 7, name: "celery", calories: 15 },
+  // { id: 8, name: "carrots", calories: 25 },
+  // { id: 9, name: "corn", calories: 63 },
+  // { id: 10, name: "broccoli", calories: 50 }];
 
 
 
   return (
     <>
+      {/* <Counter /> */}
+      <MyComponent />
       {/* <Header />
       <Food />
       <Footer />
@@ -35,15 +40,16 @@ function App() {
       <Card />
       <Card />
       <Card />
-      <Button />
-      <Student name="Spongebob" age={30} isStudent={true}/>
-      <Student name="Patrick" age={42} isStudent={false}/>
-      <Student name="Squidward" age={50} isStudent={false}/>
-      <Student name="Sandy" age={27} isStudent={true}/>
+      <Student name="Spongebob" age={30} isStudent={true} />
+      <Student name="Patrick" age={42} isStudent={false} />
+      <Student name="Squidward" age={50} isStudent={false} />
+      <Student name="Sandy" age={27} isStudent={true} />
       <Student />
-      <UserGreeting isLoggedIn={true} username="React" /> */}
+      <UserGreeting isLoggedIn={true} username="React" />
       {fruits.length > 0 && <List items={fruits} category="Fruits" />}
       {vegetables.length > 0 && <List items={vegetables} category="Vegetables" />}
+      <Button />
+      <ProfilePicture /> */}
     </>
 
   )
--- src/components/MyComponent.jsx
+++ src/components/MyComponent.jsx
@@ -2,35 +2,89 @@ import React, { useState } from 'react';
 
 function MyComponent() {
 
-    const [name, setName] = useState("Guest");
-    const [age, setAge] = useState(0);
-    const [isEmployed, setIsEmployed] = useState(false);
+    // const [name, setName] = useState("Guest");
+    // const [age, setAge] = useState(0);
+    // const [isEmployed, setIsEmployed] = useState(false);
+    const [name, setName] = useState("");
+    const [quantity, setQuantity] = useState(1);
+    const [comment, setCommment] = useState("");
+    const [payment, setPayment] = useState("");
+    const [shipping, setShipping] = useState("");
 
-    const updateName = () => {
-        setName("Spongebob");
+    function handleNameChange(event) {
+        setName(event.target.value);
     }
 
-    const incrementAge = () => {
-        //setAge(age + 1);
-        setAge(age + 3);
-        // setAge(age + 5);
+    function handleQuantityChange(event) {
+        setQuantity(event.target.value);
     }
 
-    const toggleEmployedStatus = () => {
-        setIsEmployed(!isEmployed);
+    function handleCommentChange(event) {
+        setCommment(event.target.value);
     }
+
+    function handlePaymentChange(event) {
+        setPayment(event.target.value);
+    }
+
+    function handleShippingChange(event) {
+        setShipping(event.target.value);
+    }
+
+    // const updateName = () => {
+    //     setName("Spongebob");
+    // }
+
+    // const incrementAge = () => {
+    //     //setAge(age + 1);
+    //     setAge(age + 3);
+    //     // setAge(age + 5);
+    // }
+
+    // const toggleEmployedStatus = () => {
+    //     setIsEmployed(!isEmployed);
+    // }
 
     return (
 
         <div>
-            <p>Name: {name}</p>
+            {/* <p>Name: {name}</p>
             <button onClick={updateName}>Set Name</button>
 
             <p>Age: {age}</p>
             <button onClick={incrementAge}>Increment Age</button>
 
             <p>Is Employed: {isEmployed ? "Yes" : "No"}</p>
-            <button onClick={toggleEmployedStatus}>Toggle Status</button>
+            <button onClick={toggleEmployedStatus}>Toggle Status</button> */}
+
+            <input value={name} onChange={handleNameChange}></input>
+            <p>Name: {name}</p>
+
+            <input value={quantity} onChange={handleQuantityChange} type='number'></input>
+            <p>Quantity: {quantity}</p>
+
+            <textarea value={comment} onChange={handleCommentChange} placeholder='Write something'></textarea>
+            <p>Comment: {comment}</p>
+
+            <select value={payment} onChange={handlePaymentChange}>
+                <option value="">Select an option</option>
+                <option value="Visa">Visa</option>
+                <option value="Mastercard">Mastercard</option>
+                <option value="Giftcard">Giftcard</option>
+            </select>
+            <p>Payment: {payment}</p>
+
+            <label>
+                <input type="radio" value="Pick Up" checked={shipping === 'Pick Up'} onChange={handleShippingChange} />
+                Pick Up
+            </label>
+
+            <label>
+                <input type="radio" value="Delivery" checked={shipping === 'Delivery'} onChange={handleShippingChange} />
+                Delivery
+            </label>
+            <p>Shipping: {shipping}</p>
+
         </div>
 
     )
